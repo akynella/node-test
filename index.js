dbf87d64b23576ab09e5992ec1ade488e214efc7
akynella <124163597+akynella@users.noreply.github.com>
2023-02-01 23:24:16 +0900

    should be first push

--- index.js
+++ index.js
@@ -6,6 +6,9 @@ const port = 3000
 
 app.use(cors())
 
+app.get('/', (req, res)=>{
+    res.send('Hello World!');
+})
 app.get('/sound/:name', (req, res)=>{
     const { name } = req.params;
     if(name=="dog"){
@@ -21,5 +24,5 @@ app.get('/sound/:name', (req, res)=>{
 
 
 app.listen(port, () => {
-    console.log('Example app listening on port ${port}')
+    console.log(`Example app listening on port ${port}`)
 })
